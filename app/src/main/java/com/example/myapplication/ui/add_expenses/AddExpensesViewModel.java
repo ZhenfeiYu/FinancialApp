d3b66dcf1e1280ccb7e3facd1ae1c13e7aa8a74c
Raj Vipani <50984045+rajvipani@users.noreply.github.com>
2019-11-20 17:17:25 -0500

    update

---
+++ app/src/main/java/com/example/myapplication/ui/add_expenses/AddExpensesViewModel.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.ui.add_expenses;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AddExpensesViewModel extends ViewModel {
+
+
+
+    public AddExpensesViewModel() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/view_expenses/ViewExpensesViewModel.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.ui.view_expenses;
+
+import androidx.lifecycle.ViewModel;
+
+public class ViewExpensesViewModel extends ViewModel {
+
+
+
+    public ViewExpensesViewModel() {
+
+    }
+
+
+}
