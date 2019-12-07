578c9a48bb48df2b38e92b4c4d8ad305bfbbefe1
yuzf <yuzf@bu.edu>
2019-12-07 14:46:04 -0500

    yzf

---
+++ app/src/main/java/com/example/myapplication/ui/account_info/AccountInfoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.account_info;
+
+import androidx.lifecycle.ViewModel;
+
+public class AccountInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
